2302287b2396110ee8a9e24ba74da1bef3ba2c52
Bootcamp <njackson422@gmail.com>
2017-09-22 13:45:38 -0400

    completes functionality and adds styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,132 @@
-var btnOne = Math.floor(Math.random() * 12);
-	console.log("btnOne: " + btnOne);
+var crystalGame = {
+  btnOne: 0,
+  btnTwo: 0,
+  btnThree: 0,
+  btnFour: 0,
+  winningNum: 0,
+  totalNum: 0,
+  totalWins: 0,
+  totalLosses: 0,
 
-var btnTwo = Math.floor(Math.random() * 12);
-	console.log("btnTwo: " + btnTwo);
+  reset: function() {
+    this.btnOne = Math.ceil(Math.random() * 12);
+    console.log(this.btnOne);
+    this.btnTwo = Math.ceil(Math.random() * 12);
+    console.log(this.btnTwo);
+    this.btnThree = Math.ceil(Math.random() * 12);
+    console.log(this.btnThree);
+    this.btnFour = Math.ceil(Math.random() * 12);
+    console.log(this.btnFour);
+    this.winningNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#winNum").html("<h4 id='winNum' class='text-center'>" + this.winningNum + "</h4>");
+    console.log(this.winningNum);
+    this.totalNum = 0;
+    $("#sum").html("<h4 id='sum' class='text-center'>0</h4>");
+  },
 
-var btnThree = Math.floor(Math.random() * 12);
-	console.log("btnThree: " + btnThree);
+  gamePlay: function() {
+    this.reset();
+    $("#winNum").html("<h4 id='winNum' class='text-center'>" + this.winningNum + "</h4>");
+    $( "#button-1" ).on("click", function() {
+      crystalGame.totalNum += crystalGame.btnOne;
+      $("#sum").html("<h4 id='sum' class='text-center'>" + crystalGame.totalNum + "</h4>");
+      crystalGame.winOrLose();
+      console.log(this);
+    }) 
+    $( "#button-2" ).on("click", function() {
+        crystalGame.totalNum += crystalGame.btnTwo;
+      $("#sum").html("<h4 id='sum' class='text-center'>" + crystalGame.totalNum + "</h4>");
+      crystalGame.winOrLose();
+    }) 
+    $( "#button-3" ).on("click", function() {
+        crystalGame.totalNum += crystalGame.btnThree;
+      $("#sum").html("<h4 id='sum' class='text-center'>" + crystalGame.totalNum + "</h4>");
+      crystalGame.winOrLose();
+    }) 
+    $( "#button-4" ).on("click", function() {
+        crystalGame.totalNum += crystalGame.btnFour;
+      $("#sum").html("<h4 id='sum' class='text-center'>" + crystalGame.totalNum + "</h4>");
+      crystalGame.winOrLose();
+    })
+  },
 
-var btnFour = Math.floor(Math.random() * 12);
-	console.log("btnFour: " + btnFour);
+  winOrLose: function() { 
+    if (this.totalNum === this.winningNum) {
+      this.totalWins++;
+      $("#wins").html("<h4 id='wins' class='text-center'>Wins: " + this.totalWins + "</h4>");
+      this.reset();
+    } else if (this.totalNum > this.winningNum) {
+      this.totalLosses++;
+      $("#losses").html("<h4 id='losses' class='text-center'>Losses: " + this.totalLosses + "</h4>");
+      this.reset();
+    }
+  }
+}
 
-var winningNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-	console.log("winningNum: " + winningNum);
-
-
-var totalNum; //keypress + keypress + keypress
-
-//find sum
-//A + B = C
-//C + X = Y
-//Y +....
-//jQuery Calculator example?
-
-
-var win; 
-// totalNum = winningNum
-
-var lose;
-// totalNum > winningNum
-
-var totalWins = 0;
-var totalLosses = 0;
-
-
-$( "#winNum" ).html(winningNum);
-$( "#sum" ).html(totalNum);
-$( "#wins" ).html("Wins: " + totalWins);
-$( "#losses" ).html("Losses: " + totalLosses);
+crystalGame.gamePlay();
 
 
 
 
-// reset function on lose or win
-	// -four new hidden values
-	// -generate new winningNum
-	// -reset totalNum
+
+
+
+
+
+
+
+
+
+//  ingNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+// 	console.log("winningNum: " + winningNum);
+
+
+// var totalNum = 0; //keypress + keypress + keypress
+// //find sum
+// //A + B = C
+// //C + X = Y
+// //Y +....
+// //jQuery Calculator example?
+
+// var win; 
+// // totalNum = winningNum
+
+// var lose;
+// // totalNum > winningNum
+
+// var totalWins = 0;
+// var totalLosses = 0;
+
+
+// $( "#winNum" ).html(winningNum);
+// $( "#sum" ).html(<h4 id='sum' class='text-center'>" + totalNum + "</h4> ");
+// $( "#wins" ).html("Wins: " + totalWins);
+// $( "#losses" ).html("Losses: " + totalLosses);
+
+
+
+
+
+// $( "#button-1" ).on("click", function() {
+// 	totalNum += btnOne;
+// 	console.log(totalNum);
+// }) 
+// $( "#button-2" ).on("click", function() {
+// 	totalNum += btnTwo;
+// }) 
+// $( "#button-3" ).on("click", function() {
+// 	totalNum += btnThree;
+
+// }) 
+// $( "#button-4" ).on("click", function() {
+// 	totalNum += btnFour;
+
+// }) 
+
+
+
+
+// // reset function on lose or win
+// 	// -four new hidden values
+// 	// -generate new winningNum
+// 	// -reset totalNum
